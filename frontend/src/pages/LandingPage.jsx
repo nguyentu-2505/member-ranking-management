cea24f9aad8c512ffe6b6becceb78d3b576a789a
oofie <tdkmike7406@gmail.com>
2026-01-05 22:16:20 +0700

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -11,7 +11,6 @@ const LandingPage = () => {
         <IntroSectionLanding /> 
         <AboutUsLanding />
         <GallerySection/>
-        <AboutUsLanding />
         <Footer/>
       </>
     )
